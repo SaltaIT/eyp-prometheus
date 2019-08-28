291f5e11dc2a0fbcb51526040d6262a0a389bdac
jordiprats <jordi.prats@gmail.com>
2019-08-28 09:16:51 +0200

    lint

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,13 +1,12 @@
 class prometheus::config inherits prometheus {
 
   systemd::service { $prometheus::params::service_name:
-    description => 'Prometheus Server',
+    description   => 'Prometheus Server',
     documentation => 'https://prometheus.io/docs/introduction/overview/',
-    after_units => [ 'network-online.target' ],
-
-    user => 'prometheus',
-    restart => 'on-failure',
-    execstart => "/opt/prometheus/prometheus --config.file=/opt/prometheus/prometheus.yml --storage.tsdb.path==/opt/prometheus-data",
+    after_units   => [ 'network-online.target' ],
+    user          => 'prometheus',
+    restart       => 'on-failure',
+    execstart     => "/opt/prometheus-${prometheus::version}.linux-amd64/prometheus --config.file=/opt/prometheus/prometheus.yml --storage.tsdb.path==/opt/prometheus-data",
   }
 
 }
