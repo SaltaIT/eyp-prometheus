655a68b9a342886e53f101fd706492cee7962613
jordiprats <jordi.prats@gmail.com>
2019-09-04 09:04:52 +0200

    variables prometheus

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,11 @@ class prometheus(
                             $version                = '2.12.0',
                             $include_prometheus_job = true,
                             $prometheus_job_targets = [ 'localhost:9090' ],
-                            $rules_dir              = '/etc/prometheus/rules.d',
+                            $alertmanagers          = [],
+                            $alert_rules            = [ '/etc/prometheus/rules.d/*.rules' ],
+                            $scrape_interval        = '15s',
+                            $evaluation_interval    = '15s',
+                            $scrape_timeout         = '10s',
                           ) inherits prometheus::params{
 
   class { '::prometheus::install': }
